d8c239c2bc2dfb80d6107b931e180eeaf1e60b51
Beni_BGW <118313691+benibgw@users.noreply.github.com>
2025-04-27 19:37:40 -0300

    Add files via upload

---
+++ troll.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+mensagem = "Ain lorenzooooo-chann !!!"
+quantidade = 500
+espera = 0.1
+
+print("5 segundos para ir para o whats")
+time.sleep(5)
+
+for i in range(quantidade):
+    pyautogui.typewrite(mensagem)
+    pyautogui.press('enter')
+    time.sleep(espera)
+
+print("Mensagens enviadas!")
